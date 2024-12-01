9c765f0b23d5d3eebfce7e91426c30f0490736df
Kadu Cosenza <kaducosenza@Mini-de-Kadu.lan>
2024-12-01 14:25:21 -0300

    mensagem

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
-import pandas in pd
-print('blalalalalalalal')
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles.csv')
+hist_button = st.button('Criar histograma')3
+
+car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
+fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+fig.show()
+
+st.header("Histograma de Veiculos")
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+     
+
+
+
+
+#streamlit run app.py
